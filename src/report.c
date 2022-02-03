94882644d47fd5125718248d2d685a9df941410d
danglotb <benjamin.danglot@davidson.fr>
2022-02-03 13:57:11 +0100

    feat: add duration in report

--- src/report.c
+++ src/report.c
@@ -46,10 +46,10 @@ int report_write_entry(struct perf_data_by_id entry, FILE *fptr,
     for (int i = 0 ; i < config_rapl->nb_counter; i++) {
         fprintf(fptr, "\t\t\"%s\":%ld,\n", config_rapl->counters_names[i].value, rapl_buffer->values[i].value);
     }   
-    for (int i = 0 ; i < config_perf->nb_counter - 1; i++) {
+    for (int i = 0 ; i < config_perf->nb_counter; i++) {
         fprintf(fptr, "\t\t\"%s\":%ld,\n", config_perf->counters_names[i].value, perf_buffer->values[i].value);
     }   
-    fprintf(fptr, "\t\t\"%s\":%ld\n", config_perf->counters_names[config_perf->nb_counter - 1].value, perf_buffer->values[config_perf->nb_counter - 1].value);
+    fprintf(fptr, "\t\t\"%s\":%f\n", "duration", entry.elapsedTime);
     map_remove(identifier);
     return 0;
 }
