c0cdfada253f362ef1546149984fef7c3fa0fd7f
danglotb <benjamin.danglot@davidson.fr>
2022-02-16 11:32:05 +0100

    fix: several issues on copying and managing configs

--- src/TLPCSensor.c
+++ src/TLPCSensor.c
@@ -12,7 +12,6 @@ JNIEXPORT jintArray JNICALL Java_fr_davidson_tlpc_sensor_TLPCSensor_sensorStart(
     struct config *config_rapl = init_config(_nb_rapl_counter, _rapl_counters_type);
     int perf_group_leader_fd = -1, rapl_group_leader_fd = -1;
     sensor_init(config_perf, config_rapl, 0, &perf_group_leader_fd, &rapl_group_leader_fd);
-    sensor_start(rapl_group_leader_fd, perf_group_leader_fd);
     free(config_rapl);
     free(config_perf);
     jint fds_int[2];
@@ -20,6 +19,7 @@ JNIEXPORT jintArray JNICALL Java_fr_davidson_tlpc_sensor_TLPCSensor_sensorStart(
     fds_int[1] = (int) rapl_group_leader_fd;
     jintArray fds_array = (*env)->NewIntArray(env, 2);
     (*env)->SetIntArrayRegion(env, fds_array, 0, 2, fds_int);
+    sensor_start(rapl_group_leader_fd, perf_group_leader_fd);
     return fds_array;
 }
 
@@ -30,6 +30,7 @@ JNIEXPORT jintArray JNICALL Java_fr_davidson_tlpc_sensor_TLPCSensor_sensorStart(
  */
 JNIEXPORT jlongArray JNICALL Java_fr_davidson_tlpc_sensor_TLPCSensor_sensorStop(JNIEnv *env, jobject o, int perf_group_leader_fd, int rapl_group_leader_fd)
 {
+    sensor_stop(perf_group_leader_fd, rapl_group_leader_fd);
     (void)o;
     struct config *config_perf = init_config(_nb_perf_counter, _perf_counters_type);
     struct config *config_rapl = init_config(_nb_rapl_counter, _rapl_counters_type);
@@ -39,17 +40,18 @@ JNIEXPORT jlongArray JNICALL Java_fr_davidson_tlpc_sensor_TLPCSensor_sensorStop(
     struct perf_read_format *rapl_buffer = (struct perf_read_format *)malloc(rapl_buffer_size);
     sensor_read(perf_group_leader_fd, rapl_group_leader_fd, perf_buffer, perf_buffer_size, rapl_buffer, rapl_buffer_size);
     sensor_terminate(perf_group_leader_fd, rapl_group_leader_fd);
-    free(config_rapl);
-    free(config_perf);
-    const unsigned int nb_configs = _nb_perf_counter + _nb_rapl_counter;
+    const unsigned int nb_configs = _nb_perf_counter + _nb_rapl_counter + 1;
     jlong args[nb_configs];
     for (int i = 0; i < _nb_perf_counter ; i++) {
         args[i] = (long) perf_buffer->values[i].value;
     }
     for (int i = 0; i < _nb_rapl_counter ; i++) {
-        args[i] = (long) rapl_buffer->values[i].value;
+        args[_nb_perf_counter + i] = (long) rapl_buffer->values[i].value;
     }
+    args[nb_configs - 1] = perf_buffer->time_running;
     jlongArray collectedValues = (*env)->NewLongArray(env, nb_configs);
     (*env)->SetLongArrayRegion(env, collectedValues, 0, nb_configs, args);
+    free(config_rapl);
+    free(config_perf);
     return collectedValues;
 }
